8e1971c868dc4eea7205f9fd00dc2d977d87635b
Helbert Miranda <144203312+4RE51@users.noreply.github.com>
2025-03-12 20:19:29 -0300

    Atualização da arquitetura de paginas Next.js

---
+++ organize/src/app/pages/create/page.js
@@ -0,0 +1,7 @@
+export default function create() {
+  return (
+    <div>
+      <h1>Pagina de Cadastro</h1>
+    </div>
+  );
+}
---
+++ organize/src/app/pages/profile/page.js
@@ -0,0 +1,7 @@
+export default function profile() {
+  return (
+    <div>
+      <h1>Pagina de Perfil</h1>
+    </div>
+  );
+}
