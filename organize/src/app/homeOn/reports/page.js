f8721e9007b4b66b6a83fccf7a044c9a56968ec1
Helbert Miranda <144203312+4RE51@users.noreply.github.com>
2025-03-19 02:12:39 -0300

    Desenvolvimento do Sidebar mobile e web da aplicação - Front-End Next.js

--- organize/src/app/homeOn/reports/page.js
+++ organize/src/app/homeOn/reports/page.js
@@ -1,3 +1,5 @@
+import { Button } from "../../../components/ui/button";
+
 export const metadata = {
   title: "Relatórios",
   description: "OrgaNize - Organize seu dia do jeito mais nice!",
@@ -7,6 +9,7 @@ export default function Report() {
   return (
     <div>
       <h1>Pagina de Relatório</h1>
+      <Button>Teste botão</Button>
     </div>
   );
 }
--- organize/src/app/layout.js
+++ organize/src/app/layout.js
@@ -1,5 +1,10 @@
 import "./globals.css";
-import Header from "../components/header";
+import { Inter } from "next/font/google";
+import { cn } from "../lib/utils";
+
+import { Sidebar } from "@/components/sidebar";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
@@ -9,8 +14,13 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body>
-        <Header />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.className
+        )}
+      >
+        <Sidebar />
         {children}
       </body>
     </html>
