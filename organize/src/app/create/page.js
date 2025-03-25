0d6e31ca952362c966e3e3758cde50aa12536652
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2025-03-25 19:42:54 -0300

    Adicionando resposividade na página de cadastro

--- organize/src/app/create/page.js
+++ organize/src/app/create/page.js
@@ -20,26 +20,26 @@ export default function Create() {
 
   return (
     <main className="bg-gray-900 text-white h-screen flex flex-col">
-      <div className="flex flex-row w-2/3 h-2/3 m-auto">
-        <div className="bg-gradient-to-r from-white to-gray-3500 text-bg-white flex flex-col items-center justify-center text-center mx-auto w-[50%] rounded-l-lg">
-          <div className="flex -space-x-1 overflow-hidden">
+      <div className="flex flex-row md:w-3/4 sm:w-3/4 w-3/4 h-2/3 m-auto">
+        <div className="bg-gradient-to-r from-white to-gray-3500 text-bg-white flex flex-col items-center justify-center text-center mx-auto md:w-[50%] sm:w-[30%] rounded-l-lg">
+          <div className="flex flex-wrap justify-center gap-2">
             <img
-              className="inline-block size-35 rounded-full ring-2 ring-transparent"
+              className="inline-block md:size-35 sm:size-20 size-30 rounded-full ring-2 ring-transparent"
               src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
               alt=""
             />
             <img
-              className="inline-block size-35 rounded-full ring-2 ring-transparent"
+              className="inline-block md:size-35 sm:size-20 size-30 rounded-full ring-2 ring-transparent"
               src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
               alt=""
             />
             <img
-              className="inline-block size-35 rounded-full ring-2 ring-transparent"
+              className="inline-block md:size-35 sm:size-20 size-30 rounded-full ring-2 ring-transparent"
               src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
               alt=""
             />
             <img
-              className="inline-block size-35 rounded-full ring-2 ring-transparent"
+              className="inline-block md:size-35 sm:size-20 size-30 rounded-full ring-2 ring-transparent"
               src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
               alt=""
             />
@@ -47,60 +47,62 @@ export default function Create() {
           </div>
         </div>
 
-        <div className="flex flex-col ms-0 w-[50%] p-5 gap-10 my-auto">
+        <div className="flex flex-col ms-0 w-60% sm:w-[60%] md:w-4/5 lg:w-[50%] p-5 gap-10 my-auto">
             <div className="flex flex-col justify-center items-center gap-2">
-              <a className="flex flex-row h-[50px] gap-2 border w-2/3 p-2 rounded-md justify-center items-center" href="">
-                <FcGoogle size={30} />
-                <span className="text-2xl font-bold text-start">Cadastre-se pelo Google</span>
+              <a className="flex flex-row md:h-[50px] sm:h-[20%] h-[20%] gap-2 border lg:w-2/3 md:w-full sm:w-full w-full p-2 rounded-md justify-center items-center" href="">
+                <FcGoogle className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl"/>
+                <span className="text-9 sm:text-2xl md:text-[20px] font-bold text-start">Cadastre-se pelo Google</span>
               </a>                
-              <a className="flex flex-row h-[50px] gap-2 border w-2/3 p-2 rounded-md justify-center items-center" href="">
-                <ImFacebook2 size={30} />
-              <span className="text-2xl font-bold text-start">Cadastre-se pelo Facebook</span>
+              <a className="flex flex-row md:h-[50px] sm:h-[20%] h-[20%] gap-2 border lg:w-2/3 sm:w-full w-full p-2 rounded-md justify-center items-center" href="">
+                <ImFacebook2 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl"/>
+              <span className="text-9 sm:text-2xl md:text-[20px] font-bold text-start">Cadastre-se pelo Facebook</span>
               </a>                
-              <a className="flex flex-row h-[50px] gap-2 border w-2/3 p-2 rounded-md justify-center items-center" href="">
-                <FaMicrosoft size={30} />
-                <span className="text-2xl font-bold text-start">Cadastre-se pela Microsoft</span>
+              <a className="flex flex-row md:h-[50px] sm:h-[20%] h-[20%] gap-2 border lg:w-2/3 sm:w-full w-full p-2 rounded-md justify-center items-center" href="">
+                <FaMicrosoft className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl"/>
+                <span className="text-9 sm:text-2xl md:text-[20px] font-bold text-start">Cadastre-se pela Microsoft</span>
               </a>                
             </div>
-          <Form>
-            <fieldset className="text-6xl font-bold mb-4">Cadastre-se</fieldset>
+          <Form className="flex flex-col gap-3">
+            <fieldset className="md:text-6xl sm:text-3xl text-3xl font-bold mb-4 text-center">Cadastre-se</fieldset>
             {/* On submission, the input value will be appended to
           the URL, e.g. /search?query=abc */}
-            <div className="flex flex-col">
-              <label htmlFor="name" className="mb-2">
-                Nome
-              </label>
               <input
                 type="text"
+                placeholder="Nome completo"
                 id="name"
                 name="name"
-                className="border p-2 rounded-md mb-2"
+                className="border p-2 rounded-md text-sm/5"
               />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="email" className="mb-2">
-                Email
-              </label>
               <input
                 type="email"
+                placeholder="Email (exemplo: user123@gmail.com)"
                 id="email"
                 name="email"
-                className="border p-2 rounded-md mb-2"
+                className="border p-2 rounded-md text-sm/5"
               />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="password" className="mb-2">
-                Senha
-              </label>
+              <input
+                type="email"
+                placeholder="Confirme email"
+                id="confirmEmail"
+                name="confirmEmail"
+                className="border p-2 rounded-md text-sm/5"
+              />
               <input
                 type="password"
+                placeholder="Senha"
                 id="password"
                 name="password"
-                className="border p-2 rounded-md mb-2"
+                className="border p-2 rounded-md text-sm/5"
               />
-            </div>
+              <input
+                type="password"
+                placeholder="Confirme sua senha"
+                id="confirmPassword"
+                name="confirmPassword"
+                className="border p-2 rounded-md text-sm/5"
+              />
             <div className="flex flex-row justify-center items-center mt-3">
-              <button onClick={handleSubmit} className="bg-[#ffbf00] hover:bg-[#ffd191] transition py-2 w-[50%] rounded-md text-black font-bold">Submit</button>
+              <button onClick={handleSubmit} className="bg-[#ffbf00] hover:bg-[#ffd191] transition py-2 px-4 w-[70%] sm:w-[70%] md:w-[30%] rounded-md text-black font-bold">CADASTRAR</button>
             </div>            
             </Form>
         </div>
