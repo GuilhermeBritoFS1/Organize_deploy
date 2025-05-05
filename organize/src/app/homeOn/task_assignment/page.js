73334b17733d2704ceb30a10ff8c59deb788d60e
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2025-05-05 20:23:40 -0300

    Removendo import de css de task assignment

--- organize/src/app/homeOn/task_assignment/page.js
+++ organize/src/app/homeOn/task_assignment/page.js
@@ -17,8 +17,6 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 
-//css:
-import styles from "./taskAssignment.module.css";
 
 export default function Task_assignment() {
   const tasks = [
